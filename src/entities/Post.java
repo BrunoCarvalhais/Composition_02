99cc660342d5f266689f80eae5e0cf8a9d6313bd
BrunoCarvalhais <carvalhaisazevedo@gmail.com>
2021-11-09 14:17:43 -0300

    Final Commit

--- src/entities/Post.java
+++ src/entities/Post.java
@@ -1,10 +1,13 @@
 package entities;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 public class Post {
+
+    private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyy HH:mm:ss");
 
     private Date moment;
     private String title;
@@ -63,5 +66,20 @@ public class Post {
 
     public void removeComment(Comment comment){
         comments.remove(comment);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder();
+        sb.append(title + "\n");
+        sb.append(likes);
+        sb.append(" Likes - ");
+        sb.append(sdf.format(moment) + "\n");
+        sb.append(content + "\n");
+        sb.append("Comments:\n");
+        for (Comment c: comments){
+            sb.append(c.getText() + "\n");
+        }
+        return sb.toString();
     }
 }
